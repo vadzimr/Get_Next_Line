bf057c2056241db3e449b4796446c5d12078b422
Vadzim Rayinchick <vrayinch@e1z2r2p10.42.us.org>
2020-02-28 20:54:25 -0800

    r

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,70 @@
 /*   By: vrayinch <vrayinch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 15:21:38 by vrayinch          #+#    #+#             */
-/*   Updated: 2020/02/28 13:00:07 by vrayinch         ###   ########.fr       */
+/*   Updated: 2020/02/28 20:43:48 by vrayinch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "libft/libft.h"
 
-int main(void)
+int get_next_line(int fd, char **line)
+{
+    char buf[14];
+    int bytes_read;
+
+    *line = ft_strnew(1);
+    
+    while ((bytes_read = read(fd, buf, 1)))
+    {
+        if(ft_strchr(buf, '\n'))
+        {
+            return (1);
+            break;
+        }
+        buf[bytes_read] = '\0';
+        *line = ft_strjoin(*line, buf);
+    }
+    
+return (1);
+
+}
+
+int main(int ac, char **av)
 {
 int fd;
-int fd1;
+char *str;
+int res;
 
-char buf[14];
-
-fd = open("test_file.txt", O_RDONLY, 0);
-printf("fd is: %d\n", fd);
-
-
-read(fd1, buf, 5);
-
-printf("buf: %s\n", buf);
+if (ac == 2)
+	fd = open(av[1], O_RDONLY);
+else
     return (0);
-}
+
+// while (res)
+// {
+//     res = get_next_line(fd, &str);
+//     printf("str is: %s\n", str);
+// 	ft_strdel(&str);
+// }
+get_next_line(fd, &str);
+printf("str is: %s\n", str);
+printf("\n");
+get_next_line(fd, &str);
+printf("str is: %s\n", str);
+printf("\n");
+get_next_line(fd, &str);
+printf("str is: %s\n", str);
+printf("\n");
+get_next_line(fd, &str);
+printf("str is: %s\n", str);
+printf("\n");
+
+
+
+// close(fd);
+// return (0);
+ }
 
