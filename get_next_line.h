3800323bd15e6ebb671629c435b0d4b70e64dcc2
Vadzim Rayinchick <vrayinch@e1z2r2p10.42.us.org>
2020-03-11 14:45:09 -0700

    completed_project

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,20 @@
 /*   By: vrayinch <vrayinch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 07:50:13 by vrayinch          #+#    #+#             */
-/*   Updated: 2020/02/13 15:27:13 by vrayinch         ###   ########.fr       */
+/*   Updated: 2020/03/09 17:56:54 by vrayinch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
 # include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include "libft/libft.h"
 
-# define MAX_FD 4096
-# define BUFF_SIZE 32
+# define FD_COUNT 12042
+# define BUFF_SIZE 42
 
 int	get_next_line(const int fd, char **line);
 
